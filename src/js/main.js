6543bfcf71d22503dd0fdac70289913a296e6cc5
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2022-12-26 01:51:45 -0300

    Configurações de PWA

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,23 @@ import { Elm } from '../elm/Main.elm';
 let app = Elm.Main.init({
     node: document.querySelector('#app'),
 });
+
+
+// Start service workers
+(async () => {
+    if ("serviceWorker" in navigator) {
+        // App shell SW 
+        try {
+            const registration = await navigator.serviceWorker.register("./sw.js", { scope: "/f_oda-se/" });
+            if (registration.installing) {
+                console.log("App shell SW installing");
+            } else if (registration.waiting) {
+                console.log("App shell SW installed");
+            } else if (registration.active) {
+                console.log("App shell SW active");
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+})();
